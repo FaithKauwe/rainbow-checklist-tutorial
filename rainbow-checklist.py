3311b9a94a99c9467286783b9bf72f556da7921a
FaithKauwe <fkauwe@gmail.com>
2024-09-22 19:06:32 -0700

    created helper functions

--- rainbow-checklist.py
+++ rainbow-checklist.py
@@ -28,6 +28,42 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
+# ADDS CHECKMARK
+def mark_completed(index):
+    checklist[index] = ("{} {}".format("√", checklist[index]))
+
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print("{} {}".format(index, list_item))
+        index += 1
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
 def test():
     create("purple sox")
     create("red cloak")
@@ -41,5 +77,8 @@ def test():
     print(read(0))
     print(read(1))
 
+    list_all_items()
+
 # call the test funciton
 test()
+
